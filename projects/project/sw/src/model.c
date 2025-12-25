e69401416500f8a4d642b4bb966a0542fba49e9a
mengwx2025 <mengwx2025@shanghaitech.edu.cn>
2025-12-25 22:24:16 +0800

    scalar model

---
+++ projects/project/sw/src/model.c
@@ -0,0 +1,40 @@
+#include "model.h"
+#include "trap.h"
+#include "scalar.c"
+
+int main() {
+    int8_t (*input)[14][14] = (int8_t (*)[14][14])ADDR_INPUT;
+
+    int8_t (*weightconv1)[6][3][3] = (int8_t (*)[6][3][3])ADDR_WCONV1;
+    int16_t scaleconv1 = *((int16_t *)ADDR_SCONV1);
+    int16_t (*outputconv1)[12][12] = (int16_t (*)[12][12])ADDR_OUTCONV1;
+
+    int16_t (*outputpool1)[6][6] = (int16_t (*)[6][6])ADDR_OUTPOOL1;
+
+    int16_t (*weightfc1)[144] = (int16_t (*)[144])ADDR_WFC1;
+    int32_t scalefc1 = *((int32_t *)ADDR_SFC1);
+    int32_t *outputfc1 = (int32_t *)ADDR_OUTFC1;
+
+    int32_t (*weightfc2)[60] = (int32_t (*)[60])ADDR_WFC2;
+    int32_t *biasfc2 = (int32_t *)ADDR_BFC2;
+    int32_t *outputfc2 = (int32_t *)ADDR_OUTFC2;
+
+    int32_t *softmaxlut = (int32_t *)ADDR_SOFTMAX_LUT;
+    int32_t *outputsoftmax = (int32_t *)ADDR_OUTSM2;
+
+    conv(input, weightconv1, outputconv1, scaleconv1);
+
+    maxpool(outputconv1, outputpool1);
+
+    fc1(outputpool1, weightfc1, outputfc1, scalefc1);
+
+    fc2(outputfc1, weightfc2, biasfc2, outputfc2);
+
+    softmax(outputfc2, softmaxlut,outputsoftmax);
+
+    // for (int i = 0; i < 10; i++) {
+    //     printf("%d ", outputsoftmax[i]);
+    // }
+
+    return 0; 
+}
---
+++ projects/project/sw/src/scalar.c
@@ -0,0 +1,119 @@
+#include "trap.h"
+
+void conv(int8_t input[6][14][14], int8_t weight[4][6][3][3], int16_t output[4][12][12], int16_t scale) {
+    for (int f = 0; f < 4; f++) {
+        for (int h = 0; h < 12; h++) {
+            for (int w = 0; w < 12; w++) {
+                int16_t sum = 0;
+                for (int c = 0; c < 6; c++) {
+                    for (int kh = 0; kh < 3; kh++) {
+                        for (int kw = 0; kw < 3; kw++) {
+                            sum += (int16_t)input[c][h + kh][w + kw] * (int16_t)weight[f][c][kh][kw];
+                        }
+                    }
+                }
+                output[f][h][w] = (sum > 0) ? sum / scale : 0;
+            }
+        }
+    }
+}
+
+void maxpool(int16_t input[4][12][12], int16_t output[4][6][6]) {
+    for (int c = 0; c < 4; c++) {
+        for (int h = 0; h < 6; h++) {
+            for (int w = 0; w < 6; w++) {
+                int16_t max_val = input[c][h * 2][w * 2];
+                for (int ph = 0; ph < 2; ph++) {
+                    for (int pw = 0; pw < 2; pw++) {
+                        int16_t val = input[c][h * 2 + ph][w * 2 + pw];
+                        if (val > max_val) {
+                            max_val = val;
+                        }
+                    }
+                }
+                output[c][h][w] = max_val;
+            }
+        }
+    }
+}
+
+void fc1(int16_t input[4][6][6], int16_t weight[60][144], int32_t output[60], int32_t scale) {
+    for (int i = 0; i < 60; i++) {
+        int32_t sum = 0;
+        for (int j = 0; j < 144; j++) {
+            sum += (int32_t)((int16_t *)input)[j] * (int32_t)weight[i][j];
+        }
+        output[i] = (sum > 0) ? sum / scale : 0;
+    }
+}
+
+void fc2(int32_t input[60], int32_t weight[10][60], int32_t bias[10], int32_t output[10]) {
+    for (int i = 0; i < 10; i++) {
+        int32_t sum = bias[i];
+        for (int j = 0; j < 60; j++) {
+            sum += (int32_t)input[j] * (int32_t)weight[i][j];
+        }
+        output[i] = sum;
+    }
+}
+
+void softmax(int32_t input[10], int32_t softmaxlut[256], int32_t output[10]) {
+    int32_t Q = 16;
+    int32_t Q_16 = (1 << Q);
+    int32_t LUT_SIZE = 256;
+    int32_t SAFE_SHIFT = 2;
+
+    int size = 10;
+    int32_t x_q16_16[10];
+    for (int i = 0; i < size; i++) {
+        int32_t clipped = input[i];
+        if (clipped < -32767)
+            clipped = -32767;
+        if (clipped > 32767)
+            clipped = 32767;
+        x_q16_16[i] = clipped << 16;
+    }
+
+    int32_t x_max = x_q16_16[0];
+    for (int i = 1; i < size; i++) {
+        if (x_q16_16[i] > x_max) {
+            x_max = x_q16_16[i];
+        }
+    }
+
+    int32_t exp_delta[10];
+    int32_t exp_sum = 0;
+    for (int i = 0; i < size; i++) {
+        int32_t delta = x_q16_16[i] - x_max;
+        if (delta < -8 * Q_16)
+            delta = -8 * Q_16;
+        if (delta > 0)
+            delta = 0;
+
+        int32_t idx_num = (delta + 8 * Q_16) * (LUT_SIZE - 1);
+        int32_t idx = idx_num / (8 * Q_16);
+        if (idx < 0)
+            idx = 0;
+        if (idx >= LUT_SIZE)
+            idx = LUT_SIZE - 1;
+
+        exp_delta[i] = softmaxlut[idx];
+        exp_sum += exp_delta[i];
+    }
+
+    if (exp_sum <= 0) {
+        exp_sum = 1;
+    }
+
+    int32_t exp_sum_shr = exp_sum >> SAFE_SHIFT;
+    if (exp_sum_shr <= 0) {
+        exp_sum_shr = 1;
+    }
+
+    for (int i = 0; i < size; i++) {
+        int32_t exp_delta_shr = exp_delta[i] >> SAFE_SHIFT;
+        int32_t num = exp_delta_shr * Q_16;
+        int32_t softmax_q16_16 = num / exp_sum_shr;
+        output[i] = softmax_q16_16;
+    }
+}
